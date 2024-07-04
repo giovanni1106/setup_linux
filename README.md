903423acd5dfa28cbbbf23c9f4e176469d9bb7fd
Giovanni Giampauli <giovanni.acg@gmail.com>
2024-07-04 17:38:46 -0300

    add changes

---
+++ README.md
@@ -0,0 +1,5 @@
+precisa de curl primeiro
+
+main.sh: 3: source: not found
+main.sh: 5: echo_status: not found
+
--- main.sh
+++ main.sh
@@ -6,6 +6,12 @@ echo_status "Updating the system..."
 sudo apt update
 sudo apt upgrade -y
 
+# Run initial installations scripts
+for script in initial_config/*.sh; do
+    echo_status "Running $script..."
+    bash "$script"
+done
+
 # Run installation scripts
 for script in install/*.sh; do
     echo_status "Running $script..."
